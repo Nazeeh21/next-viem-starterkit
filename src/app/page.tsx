afac04ca34f1d7958c8426e9d970294b90bace79
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2023-03-24 00:49:23 +0530

    Complete boilerplate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,99 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+"use client";
 
-import { createPublicClient, http } from 'viem'
-import { mainnet } from 'viem/chains'
+import styles from "./page.module.css";
+
+import { useState } from "react";
+import {
+  Account,
+  createPublicClient,
+  createWalletClient,
+  custom,
+  getAccount,
+  http,
+  parseEther,
+} from "viem";
+import { mainnet } from "viem/chains";
+
+declare global {
+  interface Window {
+    ethereum?: any;
+  }
+}
+
+const walletClient = createWalletClient({
+  chain: mainnet,
+  transport: custom(window.ethereum),
+});
 
 const client = createPublicClient({
   chain: mainnet,
   transport: http(),
-})
+  pollingInterval: 10_000,
+});
 
+export default function Home() {
+  const [blockNumber, setBlockNumber] = useState<null | number>(null);
+  const [connecting, setConnecting] = useState<boolean>(false);
+  const [account, setAccount] = useState<Account>();
 
-export default async function Home() {
-  const blockNumber = await client.getBlockNumber()
+  const getBlockNumber = async () => {
+    const blockNumber = await client.getBlockNumber();
+    setBlockNumber(Number(blockNumber));
+  };
 
-  console.log('blockNumber: ', blockNumber)
+  getBlockNumber();
+
+  const connectWallet = async () => {
+    setAccount(undefined);
+    setConnecting(true);
+
+    const [address] = await walletClient.requestAddresses();
+    const account = getAccount(address);
+    console.log("account: ", account);
+    setAccount(getAccount(address));
+    setConnecting(false);
+  };
+
+  const signMessage = async () => {
+    if (!account) return;
+    const message = "gm";
+
+    const signedMessage = await walletClient.signMessage({
+      account,
+      message,
+    });
+
+    console.log("signedMessage: ", signedMessage);
+    alert('Signed Msg: ' + signedMessage );
+  };
+
+  const sendTransaction = async () => {
+    if (!account) return;
+    await walletClient.sendTransaction({
+      account,
+      to: "0x0000000000000000000000000000000000000000",
+      value: parseEther("0.000000"),
+    });
+  };
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        Nextjs 13 & Viem
+      <div className={styles.content}>
+        <div>Nextjs 13 & Viem</div>
+        <div>
+          BlockNumber: <span>{blockNumber}</span>
+        </div>
+        {account ? (
+          <div>Connected address: {account.address}</div>
+        ) : connecting ? (
+          <div>Connecting...</div>
+        ) : (
+          <button onClick={connectWallet}>Connect Wallet</button>
+        )}
 
-        <div>BlockNumber: <span>{Number(blockNumber)}</span></div>
+        {account && <button onClick={signMessage}>Sign gm message</button>}
+        {account && <button onClick={sendTransaction}>Send transaction</button>}
       </div>
     </main>
-  )
+  );
 }
